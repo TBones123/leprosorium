00788a9c941e58ca3b0f115cfee4b7bda218cf43
TBones123 <33010485+TBones123@users.noreply.github.com>
2021-04-21 21:03:03 +0300

    Lesson 28 (#2)
    * add stub for /new
    
    * add new.erb view
    
    * add textarea and button
    
    * add action, method attributes to form
    
    * read content variable from post request
    
    * init database, make @db
    
    * Create table Posts

--- app.rb
+++ app.rb
@@ -2,7 +2,37 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+  @db = SQLite3::Database.new 'leprosorium.db'
+  @db.results_as_hash= true
+end
+
+before do
+  init_db
+end
+
+configure do
+  init_db
+  @db.execute 'CREATE TABLE IF NOT EXISTS Posts
+(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+created_date DATE,
+content TEXT);'
+end
+
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
+end
+
+
+get '/new' do
+  erb :new
+end
+
+post '/new' do
+  content = params[:content]
+  erb "you typed #{content}"
 end
