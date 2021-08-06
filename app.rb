d9cf249fb072c394e77f6f01423e6a0247ee22be
andrey.radushev <radushev123@gmail.com>
2021-08-06 20:39:25 +0300

    Index: get results from database

--- app.rb
+++ app.rb
@@ -24,7 +24,9 @@ end
 
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
+
+  @resaults = @db.execute 'select * from Posts order by id desc'
+	erb :index
 end
 
 
